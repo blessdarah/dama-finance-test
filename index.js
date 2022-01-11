1b4837ebc0e817690a599adc41bbc043d0886c34
blessdarah <blessdarahuba@gmail.com>
2022-01-11 10:50:03 +0100

    first package commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const initApp = function() {
+    return {
+        name: 'blessdarah',
+        email: 'blessdarahuba@gmail.com',
+        package: 'damafinance npm package'
+    };
+};
+
+module.exports = initApp;
